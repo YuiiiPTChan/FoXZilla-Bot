389334e239357e4db9f772d7416bb5b48f2ba982
FoXZillaPT17 <72317733+FoXZillaPT17@users.noreply.github.com>
2021-02-26 16:24:00 +0000

    Upload embed informations

--- player/playlistAdd.js
+++ player/playlistAdd.js
@@ -1,3 +1,16 @@
 module.exports = (client, message, queue, playlist) => {
-    message.channel.send(`${client.emotes.music} - ${playlist.title} has been added to the queue (**${playlist.tracks.length}** songs) !`);
+
+    message.channel.send({
+        embed: {
+            color: 'RANDOM',
+            author: { name: `${playlist.title} has been added to the queue!` },
+            footer: { text: 'FoXZilla-Bot' },
+            fields: [
+                { name: 'Requested by', value: playlist.requestedBy.username, inline: true },
+                { name: 'Songs', value: playlist.tracks.length, inline: true },
+            ],
+            thumbnail: { url: playlist.thumbnail },
+            timestamp: new Date(),
+        },
+    });
 };
--- player/trackAdd.js
+++ player/trackAdd.js
@@ -1,3 +1,19 @@
 module.exports = (client, message, queue, track) => {
-    message.channel.send(`${client.emotes.music} - ${track.title} has been added to the queue !`);
+    message.channel.send({
+        embed: {
+            color: 'RANDOM',
+            author: { name: `${track.title} has been added to the queue!` },
+            footer: { text: 'FoXZilla-Bot' },
+            fields: [
+                { name: 'Channel', value: track.author, inline: true },
+                { name: 'Requested by', value: track.requestedBy.username, inline: true },
+                { name: 'From playlist', value: track.fromPlaylist ? 'Yes' : 'No', inline: true },
+                { name: 'Views', value: track.views, inline: true },
+                { name: 'Duration', value: track.duration, inline: true },
+            ],
+            thumbnail: { url: track.thumbnail },
+            timestamp: new Date(),
+        },
+    });
+
 };
