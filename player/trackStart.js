38f297bb5f4428704b7b60e8f6be5e85e91ebd6a
FoXZillaPT17 <72317733+FoXZillaPT17@users.noreply.github.com>
2021-02-26 16:24:28 +0000

    Upload embed Information

--- player/trackStart.js
+++ player/trackStart.js
@@ -1,3 +1,26 @@
 module.exports = (client, message, track) => {
-    message.channel.send(`${client.emotes.music} - Now playing ${track.title} into ${message.member.voice.channel.name} ...`);
+
+    message.channel.send({
+        embed: {
+            color: 'RANDOM',
+            author: { name: `Started playing ${track.title}` },
+            footer: { text: 'FoXZilla-Bot' },
+            fields: [
+                { name: 'Channel', value: track.author, inline: true },
+                { name: 'Requested by', value: track.requestedBy.username, inline: true },
+                { name: 'From playlist', value: track.fromPlaylist ? 'Yes' : 'No', inline: true },
+
+                { name: 'Views', value: track.views, inline: true },
+                { name: 'Duration', value: track.duration, inline: true },
+
+                { name: 'Volume', value: client.player.getQueue(message).volume, inline: true },
+                { name: 'Repeat mode', value: client.player.getQueue(message).repeatMode ? 'Yes' : 'No', inline: true },
+
+                { name: 'Progress bar', value: client.player.createProgressBar(message, { timecodes: true }), inline: false }
+            ],
+            thumbnail: { url: track.thumbnail },
+            timestamp: new Date(),
+        },
+    });
+
 };
