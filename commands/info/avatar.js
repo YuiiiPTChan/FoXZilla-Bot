c0b9bae11be383b67ea5e497274beb50cf178531
Yuichiro <ruicabral11@hotmail.com>
2021-03-09 22:23:00 +0000

    Update avatar.js
    Apostofre

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -12,8 +12,8 @@ if(!message.mentions.users.size) {
 
 const author_avatar_embed = new Discord.MessageEmbed()
     .setColor('RANDOM')
-    .setAuthor(`${message.author.username}\`s Avatar`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`)
-    .setDescription('You\`re beautiful 😍💖')
+    .setAuthor(`${message.author.username}\'s Avatar`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`)
+    .setDescription('You\'re beautiful 😍💖')
     .setImage(`${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`)
     .setTimestamp()
     .setFooter(`Command executed by: ${message.author.username}`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
@@ -28,7 +28,7 @@ const avatarList = message.mentions.users.map(user => {
 const mention_user_avatar = new Discord.MessageEmbed()
     .setColor('RANDOM')
     .setAuthor(`${user.username}\`s Avatar`, `${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`, `${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`)
-    .setDescription('You\`re beautiful 😍💖')
+    .setDescription('You\'re beautiful 😍💖')
     .setImage(`${user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`)
     .setTimestamp()
     .setFooter(`Command executed by: ${message.author.username}`, `${message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })}`);
